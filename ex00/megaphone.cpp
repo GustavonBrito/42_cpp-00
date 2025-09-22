8ee53cf804ea400155ad18a761c3b85e02dba87a
GustavonBrito <gustavoserafio@gmail.com>
2025-09-21 23:46:50 -0300

    feat: initial commit, hello world in cp

---
+++ ex00/megaphone.cpp
@@ -0,0 +1,21 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   megaphone.cpp                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: gustavo <gustavo@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/09/21 22:22:04 by gustavo           #+#    #+#             */
+/*   Updated: 2025/09/21 23:46:16 by gustavo          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "mepgaphone.hpp"
+
+using namespace std;
+int main (int argc, char **argv)
+{
+	std::toupper(argv[0]);
+	cout << "Hello World!";
+	return (0);
+}
